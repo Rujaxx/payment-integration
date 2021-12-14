92e72c5d57fe3d0b475748879e46247bfb7269a9
Rujaxx <shaikhrujan@gmail.com>
2021-12-14 14:31:19 +0530

    mongodb connected

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv')
+
+// Load env vars
+dotenv.config({ path : './config/config.env'})
+
+// Database connection
+const connectDB = require('./config/db')
+connectDB()
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT,console.log(`Server listening at ${PORT}`))
