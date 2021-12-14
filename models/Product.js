be125931b8b10ce61baa2aa13073a0e1c5cbaed0
Rujaxx <shaikhrujan@gmail.com>
2021-12-14 16:38:15 +0530

    ProductSchema

---
+++ models/Product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    productname : {
+        type : String,
+        required : [true,"Please add an product name"],
+        unique : true,
+        trim : true,
+        minlength : 5
+    },
+    description : {
+        type : String,
+        required : [true,"Please add an description"],
+        trim : true,
+        minlength : 20
+    },
+    price : {
+        type : Number,
+        required: [true,"Please add a price"]
+    },
+    createdAt:{
+        type : Date,
+        default : Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: new Date()
+    }
+},{timestamps:true})
+
+
+module.exports = mongoose.model('Product',ProductSchema)
