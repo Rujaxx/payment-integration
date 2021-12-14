073eb91a6c7fd41ab417721b8472b3a625930b9a
Rujaxx <shaikhrujan@gmail.com>
2021-12-14 16:08:22 +0530

    encrypting password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     username : {
@@ -43,5 +44,14 @@ const UserSchema = new mongoose.Schema({
         default: new Date()
     }
 },{timestamps : true })
+
+// Encrypting Password
+UserSchema.pre('save' , async function(next){
+    if(!this.isModified('password')){
+        next()
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt)
+})
 
 module.exports = mongoose.model('User',UserSchema);
