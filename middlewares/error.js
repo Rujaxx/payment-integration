2d2ee10f1bb743e08deb572e9efc044c903b05ab
Rujaxx <shaikhrujan@gmail.com>
2021-12-22 16:17:04 +0530

    error rendering

--- middlewares/error.js
+++ middlewares/error.js
@@ -15,7 +15,7 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose Duplicate Key
     if (err.code === 11000){
-        const message = 'Duplicate field value entered',
+        const message = 'Duplicate field value entered'
         error = new ErrorResponse(message,400)
     }
 
@@ -25,7 +25,7 @@ const errorHandler = (err, req, res, next) => {
         error = new ErrorResponse(message,400)
     }
 
-    res.status(error.statusCode || 500).json({
+    res.status(error.statusCode || 500).render('error',{
         success: false,
         error: error.message || 'Server Error'
     })
