7ae72b24509e8d3669ce71c8c46c7a101e7270dd
Rujaxx <shaikhrujan@gmail.com>
2021-12-23 16:58:08 +0530

    paypal sdk

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const paypal = require('@paypal/checkout-server-sdk')
 const errorHandler = require('./middlewares/error')
 
 // Load env vars
@@ -21,7 +22,8 @@ const cart = require('./routes/cart')
 const app = express();
 
 // Set EJS as templating engine
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -41,6 +43,12 @@ app.use('/api/v1/products', product)
 app.use('/api/v1/orders', order)
 app.use('/api/v1/cart', cart)
 
+// app.get("/checkout", (req, res) => {
+//     res.render("checkout", {
+//       success : true,
+//       paypalClientId : process.env.PAYPAL_CLIENT_ID
+//     })
+// })
 // ErrorHandler
 app.use(errorHandler)
 
